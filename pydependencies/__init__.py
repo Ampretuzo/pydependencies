8e48fae56e94622180195bfe9e1ba0f2e11cf97a
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2015-08-11 00:31:59 -0400

    Refactoring for pip

---
+++ pydependencies/__init__.py
@@ -0,0 +1,3 @@
+from dependencies import (transitive_dependencies,
+    python_dependencies,
+    html_dependencies)
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyDependencies',
+      version='1.0',
+      description='Python Dependency Testing',
+      author='Tianhui Michael Li',
+      author_email='test@example.com',
+      url='https://github.com/tianhuil/pydependencies/',
+      packages=['pydependencies'],
+     )
