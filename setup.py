48a56ba35ee87bde8882ba533242a51ebf750eab
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2015-08-11 22:14:08 -0400

    Tweaking setup after CR

--- setup.py
+++ setup.py
@@ -2,11 +2,23 @@
 
 from distutils.core import setup
 
-setup(name='PyDependencies',
-      version='1.0',
-      description='Python Dependency Testing',
-      author='Tianhui Michael Li',
-      author_email='test@example.com',
-      url='https://github.com/tianhuil/pydependencies/',
-      packages=['pydependencies'],
-     )
+setup(
+    name='PyDependencies',
+    version='1.0',
+    description='Python Dependency Testing',
+    author='Sebastian Kreft, Tianhui Michael Li',
+    author_email='test@example.com',
+    url='https://github.com/tianhuil/pydependencies/',
+    packages=['pydependencies'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
